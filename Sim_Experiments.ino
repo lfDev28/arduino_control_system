a413f807462e1f580a058688de721f852da6d2d7
Lee Forster <lee.forster28@gmail.com>
2023-05-17 08:50:14 +1000

    Early commit

--- Sim_Experiments.ino
+++ Sim_Experiments.ino
@@ -1,120 +1,172 @@
 volatile int count;
-int ENA = 10; // Enable Pin
+int ENA = 10;      // Enable Pin
 int TEST_LED = 11; // Test LED Pin
-int IN1 = 9; // IN1 Pin
-int IN2 = 8; // IN2 Pin
-int INTA = 6; // Interrupt A Pin
-int INTB = 7; // Interrupt B Pin
-int SENSE_1 = 22; // Ball sensor read pin
+int IN1 = 9;       // IN1 Pin
+int IN2 = 8;       // IN2 Pin
+int INTA = 6;      // Interrupt A Pin
+int INTB = 7;      // Interrupt B Pin
+int SENSE_1 = 22;  // Ball sensor read pin
 unsigned long lastSerialTime = 0;
 const unsigned long serialInterval = 100;
 bool motorOn = false;
 unsigned long motorStartTime = 0;
 int beamEnd = 985;
 int beamStart = 82;
-const desiredPosition = 0;
+float desiredPosition = 0;
+int vOut = 255;
+float storedPosition = 0;
+unsigned long positionTimer = 0;
 
-void setup() {
+
+void setup()
+{
   Serial.begin(9600);
-  // Initialising Pins  
-  pinMode(IN2, OUTPUT);
-  pinMode(IN1, OUTPUT); 
-  pinMode(ENA, OUTPUT); 
-  pinMode(SENSE_1, INPUT);
-
-  // Creating interrupts
-  attachInterrupt(digitalPinToInterrupt(INTA), ISR_A, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(INTB), ISR_B, CHANGE);
+  // Initialising Pins  and Interrupts
+  pinSetup();
+  // motorBackwards(100);
 }
 
-//Test comment
+// Test comment
 
+void loop()
+{
 
-
-
-void loop() {
+// Block to periodically store the position for delay between updates.
+  
 
   // If statement using millis to store the old position and tune it.
-
-  if(degrees > 30 || degrees < -30){
-    vOut = 0;
-  }
 
   float position = analogRead(SENSE_1);
 
   float mappedPos = map(position, 64, 981, -825, 825);
 
-  float degrees = map(count, 0, 540, 0, 6.28);
+  // float degrees = map(count, 0, 540, 0, 360);
 
-  float vp = (desiredPosition - pos)*-100;
-  float vd =  vdVar * kD;
-  float vdd = degrees * kDD;
 
-  float vOut = vp + vd + vdd;
+  // Serial.println(degrees);
+
+
+  // if(degrees >= 5 || degrees <= -5) {
+  //   motorOff();
+  // }
+
+
+  // Currently gives us a value between -8.18 and +8.18
+  float vp = (desiredPosition - mappedPos)*0.01 ;
   
+  Serial.println(vp);
 
+  // Controlling the directions, the magnitudes come fron the map function
+  if(vp < 0) {
+    motorBackwards(map(vp, 0, -8.1, 0, 150));
+  } 
 
+  if(vp > 0) {
+    motorForwards(map(vp, 0, 8.1, 0, 150));
+  }
+  
+  // float vd = vdVar * kD;
+  // float vdd = degrees * kDD;
 
-  // Control the beam angle using if statements
+  // float vOut = vp + vd + vdd;
+
+  if(millis() - positionTimer >= 10) {
+    positionTimer = millis();
+    storedPosition = mappedPos;
+  }
 
 }
-
-
-
-
 
 // Interrupt Service Routine A
-void ISR_A() {
-if(digitalRead(INTA) == HIGH){
-    if (digitalRead(INTB) == LOW){
-        count++;
-    } else {
+void ISR_A()
+{
+  if (digitalRead(INTA) == HIGH)
+  {
+    if (digitalRead(INTB) == LOW)
+    {
+      count++;
+    }
+    else
+    {
       count--;
     }
   }
-  if(digitalRead(INTA) == LOW) {
-    if(digitalRead(INTB) == HIGH) {
+  if (digitalRead(INTA) == LOW)
+  {
+    if (digitalRead(INTB) == HIGH)
+    {
       count++;
-    } else {
+    }
+    else
+    {
       count--;
     }
   }
 }
 
-// Interrupt Service Routine B
-void ISR_B() {
-if(digitalRead(INTB) == HIGH){
-    if (digitalRead(INTA) == LOW){
-        count--;
-    } else {
-      count++;
-    }
-  }
-  if(digitalRead(INTB) == LOW) {
-    if(digitalRead(INTA) == HIGH) {
+// // Interrupt Service Routine B
+void ISR_B()
+{
+  if (digitalRead(INTB) == HIGH)
+  {
+    if (digitalRead(INTA) == LOW)
+    {
       count--;
-    } else {
+    }
+    else
+    {
       count++;
     }
   }
-
+  if (digitalRead(INTB) == LOW)
+  {
+    if (digitalRead(INTA) == HIGH)
+    {
+      count--;
+    }
+    else
+    {
+      count++;
+    }
+  }
 }
 
 // Function for turning off the motor
-void motorOff() {
-    analogWrite(10, 0);
+void motorOff()
+{
+  analogWrite(10, 0);
 }
 
 // Function to turn the motor clockwise
-void motorForwards(float voltage) {
+void motorForwards(int voltage)
+{
+  Serial.println("FORWARDS");
+  
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
   analogWrite(ENA, voltage);
-  digitalWrite(8, HIGH);
-  analogWrite(9, LOW);
+  
 }
 
 // Function to turn the motor anti-clockwise
-void motorBackwards(float voltage) {
+void motorBackwards(int voltage)
+{
+  Serial.println("BACKWARDS");
+  
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
   analogWrite(ENA, voltage);
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
+  
+}
+
+void pinSetup()
+{
+  pinMode(IN2, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(SENSE_1, INPUT);
+
+  // Creating interrupts
+  attachInterrupt(digitalPinToInterrupt(INTA), ISR_A, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(INTB), ISR_B, CHANGE);
 }
