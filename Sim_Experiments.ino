7fbe7e88aec5c871b1486a9d2cf2f70a78f0f824
Lee Forster <lee.forster28@gmail.com>
2023-05-22 19:10:17 +1000

    Finished controller with python number entry

--- Sim_Experiments.ino
+++ Sim_Experiments.ino
@@ -6,53 +6,90 @@ int IN2 = 8;       // IN2 Pin
 int INTA = 6;      // Interrupt A Pin
 int INTB = 7;      // Interrupt B Pin
 int SENSE_1 = 22;  // Ball sensor read pin
-unsigned long lastSerialTime = 0;
-const unsigned long serialInterval = 100;
-bool motorOn = false;
 float desiredPosition = 0;
 float storedPosition = 0;
 unsigned long positionTimer = 0;
-float kP = 0.01;
-float kD = -0.007;
-float kDD = -0.09;
+unsigned long controlTimer = 0;
+float kP = 0.0077;
+float kD = -0.25;
+float kDD = -0.32;
 float vp = 0;
 float vd = 0;
 float vdd = 0;
 float vOut = 0;
+float mappedPos = 0;
+float position = 0;
+float degrees = 0;
+float pyOutput = 0;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.setTimeout(1);
   // Initialising Pins  and Interrupts
   pinSetup();
+  // Getting intitial values
+    position = analogRead(SENSE_1);
+    mappedPos = map(position, 93, 976, -865, 865);
+    degrees = map(count, 0, 540, 0, 360);
+    storedPosition = mappedPos;
 }
 
-// Test comment
-
-void loop()
-{
-  float position = analogRead(SENSE_1);
-
-  float mappedPos = map(position, 90, 970, -825, 825);
-
-  float degrees = map(count, 0, 540, 0, 360);
-
-  if(millis() - positionTimer >= 5) {
-    positionTimer = millis();
+void loop(){
+  if(millis() - controlTimer >= 5 ) {
+    controlTimer = millis();
+    doMeasurements();
+    doControl();
     storedPosition = mappedPos;
+    readImage();
   }
 
-  if(abs(storedPosition - mappedPos) > 700) {
+// Controlling the direction
+  if(vOut < 0) {
+    motorBackwards(map(vOut, 0, -15, 150, 255));
+  } 
+
+  if(vOut > 0) {
+    motorForwards(map(vOut, 0, 15, 150, 255));
+  }
+
+  // Controlling between 30 degrees
+  if(degrees >= 30 || degrees <= -30) {
+    motorOff();
+  }
+}
+
+void doControl() {
+  vp = (desiredPosition - mappedPos)*kP;
+  vd = (mappedPos - storedPosition)*kD;
+  vdd = degrees * kDD;
+
+  vOut = vp + vd + vdd;
+}
+
+void doMeasurements() {
+  position = analogRead(SENSE_1);
+  mappedPos = map(position, 93, 976, -865, 865);
+  degrees = map(count, 0, 540, 0, 360);
+   if(abs(mappedPos - storedPosition) >= 200) {
     mappedPos = storedPosition;
   }
+}
 
+void readImage() {
+  if(Serial.available()){
+    pyOutput = Serial.readString().toFloat(); 
+    if(pyOutput >= 800) {
+      pyOutput = 800;
+    } 
+    if(pyOutput <= -800) {
+      pyOutput = -800;
+    }
+    desiredPosition = pyOutput;
+  }
+}
 
-  // Controllers
-  vp = (desiredPosition - mappedPos)*kP;
-  vd = (desiredPosition - storedPosition)*kD;
-  vdd = degrees * kDD;
-  vOut = vp + vd + vdd;
-
+void doPrint() {
   Serial.print(position);
   Serial.print("     ");
   Serial.print(degrees);
@@ -65,28 +102,6 @@ void loop()
   Serial.print("     ");
   Serial.print(vOut);
   Serial.println("");
-
-
-
-// Controlling the direction
-  if(vOut < 0) {
-    motorBackwards(map(vOut, 0, -4, 100, 255));
-  } 
-
-  if(vOut > 0) {
-    motorForwards(map(vOut, 0, 4, 100, 255));
-  }
-
-
-
-  // Jump tolerance if statement
-
-
-    if(degrees >= 30 || degrees <= -30) {
-    motorOff();
-  }
-
-
 }
 
 // Interrupt Service Routine A
@@ -144,14 +159,12 @@ void ISR_B()
 }
 
 // Function for turning off the motor
-void motorOff()
-{
+void motorOff() {
   analogWrite(10, 0);
 }
 
 // Function to turn the motor clockwise
-void motorForwards(int voltage)
-{ 
+void motorForwards(int voltage) { 
   digitalWrite(8, LOW);
   digitalWrite(9, HIGH);
   analogWrite(ENA, voltage);
@@ -159,16 +172,13 @@ void motorForwards(int voltage)
 }
 
 // Function to turn the motor anti-clockwise
-void motorBackwards(int voltage)
-{
+void motorBackwards(int voltage) {
   digitalWrite(8, HIGH);
   digitalWrite(9, LOW);
   analogWrite(ENA, voltage);
-  
 }
 
-void pinSetup()
-{
+void pinSetup() {
   pinMode(IN2, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(ENA, OUTPUT);
